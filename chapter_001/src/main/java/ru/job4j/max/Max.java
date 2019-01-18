e86561854827d7722aa1d2d6cdc12ea8f42e1def
AleksandrFt <sanekorn@yandex.com>
2019-01-18 08:39:40 +0300

    4.2. Максимум из двух чисел.

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.max;
+
+/**
+ * @author afateev
+ * @since 1.0
+ * @version 1.0
+ */
+
+public class Max {
+    public int max(int first, int second) {
+        return first >= second ? first : second;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author afateev
+ * @version 1.0
+ * @since 1.0
+ */
+public class MaxTest {
+
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenFirstEqualsSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(3, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenFirstMoreSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(3, 1);
+        assertThat(result, is(3));
+    }
+}
