d073f1b0e7db25aa078fcb0276063fc3b8fdf74f
AleksandrFt <sanekorn@yandex.com>
2019-01-21 21:43:10 +0300

    5.5. Рефакторинг кода.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Class draw pyramid.
  * @author afateev
@@ -9,17 +11,11 @@ package ru.job4j.loop;
 
 public class Paint {
 
-    /**
-     * Method drawing right side of piramid.
-     * @param height Высота пирамиды.
-     * @return Строка.
-     */
-    public String rightTrl(int height) {
+    public String loopBy(int height, int width, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int width = height;
         for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= column) {
+            for (int column =0; column != width; column++) {
+                if(predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -30,45 +26,29 @@ public class Paint {
         return screen.toString();
     }
 
-    /**
-     * Method drawing left side of piramid.
-     * @param height Высота.
-     * @return Строка.
-     */
     public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int width = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= width - column - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
     }
 
-    /**
-     * Methot drawing full piramid.
-     * @param height Высота пирамиды.
-     * @return Строка.
-     */
-    public String piramid(int height) {
-        StringBuilder screen = new StringBuilder();
-        int width = 2 * height - 1;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column){
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
+
+    public String piramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+
 }
