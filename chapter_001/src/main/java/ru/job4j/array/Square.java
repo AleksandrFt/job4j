36216e51eab35187cd2a941b766a72c35bcfbe53
AleksandrFt <sanekorn@yandex.com>
2019-01-22 04:54:12 +0300

    6.0. Заполнить массив степенями чисел. v.2

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -17,9 +17,8 @@ public class Square {
      */
     public int[] calculate (int bound) {
         int[] rst = new int[bound];
-        int first = 1;
-        for (int index = 0; index < bound; index++, first++) {
-            rst[index] = first * first;
+        for (int index = 0; index < bound; index++) {
+            rst[index] = (index + 1) * (index + 1);
         }
         return rst;
     }
