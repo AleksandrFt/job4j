a1da7cf0e9e9f712d345624f32165661f1f22c5e
AleksandrFt <sanekorn@yandex.com>
2019-02-12 00:37:34 +0300

    add class Item

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,39 @@
+package ru.job4j.tracker;
+
+
+
+public class Item {
+    private String id;
+    private String name;
+    private String description;
+    public long create;
+
+    public Item() {
+    }
+
+    public Item (String name, String description, long create) {
+        this.name = name;
+        this.description = description;
+        this.create = create;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public long getCreate() {
+        return this.create;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
