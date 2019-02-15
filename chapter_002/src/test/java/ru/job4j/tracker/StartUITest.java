cd1424b3e49bae8f41c3cffe1c99d8904c6f4bf9
AleksandrFt <sanekorn@yandex.com>
2019-02-15 04:29:59 +0300

    add StartUITest test for addItem

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,7 +2,8 @@ package ru.job4j.tracker;
 
 public class StartUITest {
     public static void main(String[] args) {
-        Input input = new StubInput(new String[] {"Create stub task."});
-        new StartUI(input).init();
+        Input input = new StubInput(new String[] {"1", "первая заявка из тестового класса!", "Описание к первой заявке из тестового класса", "2", "0"});
+        Tracker tracker = new Tracker();
+        new StartUI(input, tracker).init();
     }
 }
