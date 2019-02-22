f41acb58ec3bc2e4196cb133e90941186b4a1b75
AleksandrFt <sanekorn@yandex.com>
2019-02-22 07:51:08 +0300

    StartUITest - tests for showAll, findById, findByName

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,10 +1,30 @@
 package ru.job4j.tracker;
 
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class StartUITest {
+
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("Execute before method.");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("Execute after method.");
+    }
 
     @Test
     public void whenUserAddItemThenTrackerHasNewItemSameName() {
@@ -31,5 +51,43 @@ public class StartUITest {
                 "3", "12345", "новое имя первой заявки", "новое описание первой заявки", "0"});
         new StartUI(input, tracker).init();
         assertThat(tracker.getAll()[0].getName(), is("новое имя первой заявки"));
+    }
+
+    @Test
+    public void whenUserChoseShowAllItems() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("первая заявка", "первое описание")).setId("12345");
+        tracker.add(new Item("вторая заявка", "второе описание")).setId("67890");
+        Input input = new StubInput(new String[]{"2", "0"});
+        new StartUI(input, tracker).init();
+        String string = new String(out.toByteArray());
+        assertTrue(string.contains("Имя : первая заявка Описание : первое описание ID : 12345")
+                && string.contains("Имя : вторая заявка Описание : второе описание ID : 67890")
+        );
+    }
+
+    @Test
+    public void whenUserChoseFindById() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("первая заявка", "первое описание")).setId("12345");
+        Input input = new StubInput(new String[]{"5", "12345", "0"});
+        new StartUI(input, tracker).init();
+        String string = new String(out.toByteArray());
+        assertTrue(string.contains(tracker.getAll()[0].getName())
+        );
+    }
+
+    @Test
+    public void whenUserChoseFindAllByName() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("первая", "первое описание")).setId("12345");
+        tracker.add(new Item("вторая", "второе описание")).setId("54321");
+        tracker.add(new Item("первая", "третье описание")).setId("67890");
+        Input input = new StubInput(new String[]{"6", "первая", "0"});
+        new StartUI(input, tracker).init();
+        String string = new String(out.toByteArray());
+        assertTrue(string.contains("12345")
+                && string.contains("67890")
+        );
     }
 }
