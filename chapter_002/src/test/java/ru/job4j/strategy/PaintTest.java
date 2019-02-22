55ce6d61ef4d03903964cbdd6d83c20873bbe942
AleksandrFt <sanekorn@yandex.com>
2019-02-22 04:54:00 +0300

    Refactor PaintTest

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -8,11 +10,23 @@ import static org.junit.Assert.assertThat;
 
 public class PaintTest {
 
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("Execute before method.");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("Execute after method.");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                 .append("*********\n")
@@ -21,7 +35,21 @@ public class PaintTest {
                 .append("*       *\n")
                 .append("*********\n")
                 .append(System.lineSeparator())
-                .toString()));
-        System.setOut(stdout);
+                .toString())
+        );
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                .append("    *    \n")
+                .append("   * *   \n")
+                .append("  *   *  \n")
+                .append(" *     * \n")
+                .append("*********\n")
+                .append(System.lineSeparator())
+                .toString())
+        );
     }
 }
