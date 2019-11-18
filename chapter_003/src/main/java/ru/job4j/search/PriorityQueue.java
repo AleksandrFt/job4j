b6b213a01991a062c9e25a300f150238dd3b1d2e
AleksandrFt <sanekorn@yandex.com>
2019-11-18 03:16:20 +0300

    2. Очередь с приоритетом на LinkedList

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -7,30 +7,14 @@ public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
-        if (tasks.size() == 0) {
-            tasks.addFirst(task);
-        }
-        if (tasks.size() == 1) {
-            if (task.getPriority() < tasks.get(0).getPriority()) {
-                tasks.addFirst(task);
-            } else {
-                tasks.addLast(task);
-            }
-        }
-
-        for (int x = 1; x < tasks.size(); x++) {
-            if (task.getPriority() < tasks.get(x - 1).getPriority()) {
-                tasks.addFirst(task);
-                break;
-            } else if (task.getPriority() > tasks.get(x - 1).getPriority()
-                    && task.getPriority() < tasks.get(x).getPriority()) {
-                tasks.add(x, task);
-                break;
-            } else if (task.getPriority() > tasks.get(x).getPriority() && x == tasks.size() - 1) {
-                tasks.addLast(task);
+        int index = 0;
+        while (index < tasks.size()) {
+            if (task.getPriority() < tasks.get(index).getPriority()) {
                 break;
             }
+            index++;
         }
+        tasks.add(index, task);
     }
 
     public Task take() {
